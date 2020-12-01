052823073ff308e6585fb6610b92339249563d7f
whoisrysen <rysenwantssomespam@gmail.com>
2020-12-01 16:10:14 +0100

    update lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ci: {
+        collect: {
+            url: ['http://localhost:3000'],
+            startServerComman: 'node index.js'
+        },
+        upload: {
+            target: 'lhci',
+            token: 'fb41c7d4-c665-4397-bab4-e13bbd6381ca',
+            serverBaseUrl: 'http://localhost:9001'
+        }
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 var request = require("supertest");
-var app = require("../index.js");describe("GET /", function() {
-    it("respond with hello world", function(done) { //navigate to root and check the the response is "hello world"
-    request(app).get("/").expect("hello world", done);
-    });
+var app = require("../index.js");
+
+describe("GET /", function() {
+    it("respond with hello world", 
+        function(done) { //navigate to root and check the the response is "hello world"
+            request(app).get("/").expect("hello world", done);
+        });
 });
+
+process.exit();
