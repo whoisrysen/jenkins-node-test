482b31a0a065bd8ef38602dcd6993d6f5dcfb5f1
whoisrysen <rysenwantssomespam@gmail.com>
2020-12-07 13:54:41 +0100

    jenkins env

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,10 +8,15 @@ if (process.env.AS_CRON === "true") {
 
     process.env.LHCI_BUILD_CONTEXT__CURRENT_HASH = hash;
     process.env.LHCI_BUILD_CONTEXT__COMMIT_TIME = new Date().toISOString();
-    process.env.LHCI_BUILD_CONTEXT__CURRENT_BRANCH = 'Cron';
+    process.env.LHCI_BUILD_CONTEXT__CURRENT_BRANCH = 'cron';
     process.env.LHCI_BUILD_CONTEXT__COMMIT_MESSAGE = msg;
     process.env.LHCI_BUILD_CONTEXT__AUTHOR = 'Jenkins';
     process.env.LHCI_BUILD_CONTEXT__AVATAR_URL = 'https://avatars1.githubusercontent.com/u/71597567?s=460&v=4';
+    process.env.LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL = process.env.BUILD_URL;
+}
+else {
+    process.env.LHCI_BUILD_CONTEXT__CURRENT_BRANCH = process.env.GIT_BRANCH;
+    process.env.LHCI_BUILD_CONTEXT__CURRENT_HASH = process.env.GIT_COMMIT;
     process.env.LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL = process.env.BUILD_URL;
 }
 
